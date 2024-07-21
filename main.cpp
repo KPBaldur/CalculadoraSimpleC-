b622c22a08c679589bed82c4c74e12d61cfa2804
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-07-21 16:26:07 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <cmath>
-/*#include "aritmetica.h"
-#include "aritmetica.cpp"
-*/
+
 using namespace std;
 
 int main()
@@ -58,32 +56,4 @@ int main()
     } while (opcion != 5);
 
         return 0;
-    }
-
-/*
-    float a = 2;
-    float b = 1;
-
-    int option = 0;
-    cout << "Ingresa una operacion: ";
-    cin >> option;
-    switch (option)
-    {
-    case 1:
-        cout << "La suma de los numeros es: " << a + b;
-        break;
-    case 2:
-        cout << "La resta de los numeros es " << a - b;
-        break;
-    case 3:
-        cout << "La multi de los numeros es " << a * b;
-        break;
-    case 4:
-        cout << "La divi de los numeros es " << a / b;
-        break;
-    default:
-        cout << "esta opcion no existe";
-        break;
-        return 0;
-    }
-*/
+};
